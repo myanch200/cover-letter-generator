a5bbe9c8e89a4fa736d6b51ac3dd59827add697a
Martin Yanchev <myanch200@gmail.com>
2021-11-09 01:02:46 +0000

    update: refactoring the code

--- covergy/generate_pdf.py
+++ covergy/generate_pdf.py
@@ -1,12 +1,36 @@
+import jinja2
 import pdfkit
-class PdfGenerator:
-    """Generate pdf file from html"""
-    def __init__(self, html_file):
-        self.html_file = html_file
-        self.pdf_file = html_file.replace('.html', '.pdf')
+from pathlib import Path
+def get_filename(filename):
+    return filename.replace(" ", "_")
 
-    def generate(self):
-        """Generate pdf file"""
-        import pdfkit
-        pdfkit.from_file(self.html_file, self.pdf_file)
-        print('PDF file generated:', self.pdf_file)
+def generate_pdf(filename = "bob"):
+    html_path = Path("html")
+    html_file = filename.replace(".pdf", ".html")
+    html_full = html_path / html_file
+    pdf_path = Path("pdf")
+    full_path = pdf_path / filename
+    
+    pdfkit.from_file(str(html_full), str(full_path))
+
+
+
+def generate_html(app_name = "covergy",html_file = "default.html",user_name = "Bob",**kwargs):
+    filename = get_filename(user_name)
+    templateLoader = jinja2.FileSystemLoader(searchpath="./")
+    templateEnv = jinja2.Environment(loader=templateLoader)
+    TEMPLATE_FILE = f"./{app_name}/templates/{html_file}"
+    template = templateEnv.get_template(TEMPLATE_FILE)
+    outputText = template.render(**kwargs)
+    data_folder = Path("html")
+    filename = filename+'.html'
+    html_file = open(data_folder / filename, 'w+')
+    html_file.write(outputText)
+    html_file.close()
+
+def generate_output(user_name):
+    generate_html(user_name=user_name,name=user_name)
+    filename = get_filename(user_name)+'.pdf'
+    generate_pdf(filename=filename)
+
+generate_output("Martin Yanchev")
